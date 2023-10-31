5c90ea0c229c766bd86aa4f1210336bf21fab0b1
Vinicius Santini <vinicius2000.santini@hotmail.com>
2023-10-31 13:26:55 -0300

    project converted to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
 function App() {
     return <h1>Test</h1>;
 }
 
 const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(<App />);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+
 module.exports = {
+    entry: "./src/index.tsx",
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|ts)x?$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
@@ -17,4 +20,23 @@ module.exports = {
             filename: './index.html',
         }),
     ],
+    resolve: {
+        modules: [__dirname, "src", "node_modules"],
+        extensions: ["*", ".js", ".jsx", ".tsx", ".ts"]
+    },
+    // module: {
+    //     // rules: [
+    //     //     {
+    //     //         test: /\.jsx?$/,
+    //     //         exclude: /node_mmodules/,
+    //     //         use: ["style-loader", "css-loader"]
+    //     //     },
+    //     // {
+    //     //  test: /\.(png|svg|jpg|gif)$/,
+    //     //  exclude: /node_modules/,
+    //     //  use: ["file-loader"]   
+    //     // }
+
+    //     // ]
+    // }
 };
